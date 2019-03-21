f9df1442da1ec96aa3c5acdc29ef439d198f27d2
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-20 17:13:50 -0700

    added switch statement for executing code based on command entered (or default in case command entered is not valid)

--- liri.js
+++ liri.js
@@ -10,3 +10,17 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 
 var search = process.argv.slice(3).join(" ");
+
+switch (command) {
+    case "concert-this":
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        break;
+
+}
