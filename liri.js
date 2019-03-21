7e9e9655d1d106b80d350ca8c8c04c7b6a95c978
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-20 17:08:21 -0700

    add code for taking in user input from arguments

--- liri.js
+++ liri.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-var axios = require("axios");
-var moment = require("moment");
-var Spotify = require("node-spotify-api");
+var command = process.argv[2];
+
+var search = process.argv.slice(3).join(" ");
